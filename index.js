f07f7a3ae8a0e3aa1df11dbe3598b98736b3f11f
Minhaj-Rabby <minhaj12113@gmail.com>
2022-05-30 00:32:39 +0600

    Put method for store email and name

--- index.js
+++ index.js
@@ -125,6 +125,28 @@ async function run() {
             }
         });
 
+        //Put method for store email and name
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                option
+            );
+            const token = jwt.sign(
+                { email: email },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "24h" }
+            );
+            res.send({ result, token });
+        });
+
       
     } finally {
     }
